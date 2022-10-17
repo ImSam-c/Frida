a4c63a17150c42b06b68c6590d8838d28fc12436
Chuy Landar <66813976+ChuyLandar@users.noreply.github.com>
2022-10-17 14:49:55 -0500

    Contact-us layout and validation for email input

---
+++ public/contact-us/index.js
@@ -0,0 +1,28 @@
+const button = document.querySelector(".button");
+const username = document.querySelector("input[name='name']");
+const email = document.querySelector("input[name='email']");
+const message = document.getElementById("message");
+const inputs = document.querySelectorAll("input");
+
+email.addEventListener("change", (e) => {
+  color = validateEmail(e.target.value) ? "green" : "red";
+  e.target.style.border = "2px solid " + color;
+});
+
+function validateEmail(emailInput) {
+  const mailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (emailInput.match(mailRegex)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+button.addEventListener("click", () => {
+  if (validateEmail(email.value)) {
+    const email = 'support.frida@gmail.com';
+    location.href = "mailto:" + email + "?body=" + message.value;
+  } else {
+    //some alert like sweetAlert2
+  }
+});
