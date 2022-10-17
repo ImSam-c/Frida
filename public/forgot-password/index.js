c2175f613bdfd9ab113dbf91be1734212ba5c348
Chuy Landar <66813976+ChuyLandar@users.noreply.github.com>
2022-10-17 13:00:58 -0500

    Forgot-password layout and validation

---
+++ public/forgot-password/index.js
@@ -0,0 +1,49 @@
+const button = document.querySelector(".button");
+const email = document.querySelector("input[name='email']");
+
+email.addEventListener("change", (e) => {
+  color = validateEmail(e.target.value) ? "green" : "red";
+  e.target.style.border = "2px solid " + color;
+});
+
+function validateEmail(emailInput) {
+  const mailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (emailInput.match(mailRegex)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+async function sendData(email) {
+  // fetch url may change.
+  const response = await fetch("http://localhost:5000/api/recoverPassword", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email
+    }),
+  });
+
+  response
+    .json()
+    .then(() => {
+      alert("Recovery email link sent. Check your inbox.")
+    })
+    .catch((error) => {
+      alert("Something went wrong, try again.");
+      console.log(error);
+    });
+}
+
+button.addEventListener("click", () => {
+  if (validateEmail(email.value)) {
+    // sendData(email.value);
+    console.log("Send password reset email");
+  } else {
+    //some alert like sweetAlert2
+  }
+});
