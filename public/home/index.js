075c0d56abc95a748933ecd652c5e8894698a172
Chuy Landar <66813976+ChuyLandar@users.noreply.github.com>
2022-11-01 15:58:56 -0600

    mobile exam sample image in the home page

--- public/home/index.js
+++ public/home/index.js
@@ -1,5 +1,10 @@
 signInButton = document.querySelector(".nav-signin-button");
+signUpButton = document.querySelector(".nav-signup-button");
 
 signInButton.addEventListener("click", () => {
     location.href = "../sign-in/index.html";
+})
+
+signUpButton.addEventListener("click", () => {
+    location.href = "../sign-up/index.html";
 })
