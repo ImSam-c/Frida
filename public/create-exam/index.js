ae9becef400d73697c782348f3eab2d9d9861407
Chuy Landar <66813976+ChuyLandar@users.noreply.github.com>
2022-11-22 12:40:26 -0600

    Answer selection validation in create-exam page

--- public/create-exam/index.js
+++ public/create-exam/index.js
@@ -62,6 +62,16 @@ function getData() {
   return { questions, comments };
 }
 
+function areAllAnswersSelected(questions){
+  let result = true;
+  questions.forEach(({correctAnswer}) => {
+    if(correctAnswer === -1){
+      result = false;
+    }
+  });
+  return result;
+}
+
 async function sendData(questions, comments) {
   const response = await fetch("http://localhost:8080/api/exams/createExam", {
     method: "POST",
@@ -111,7 +121,7 @@ createButtons.forEach((createButton) => {
 saveButtons.forEach((saveButton) => {
   saveButton.addEventListener("click", () => {
     const { questions, comments } = getData();
-    if (questions.length > 0) {
+    if (areAllAnswersSelected(questions) && questions.length > 0) {
       Swal.fire({
         title: "Are you sure?",
         icon: "question",
@@ -126,7 +136,17 @@ saveButtons.forEach((saveButton) => {
       }).then((result) => {
         result.isConfirmed ? sendData(questions, comments) : null;
       });
-    }
+    } else{
+        Swal.fire({
+          title: "Hey!",
+          html: '<p class="modal-font">Select a correct answer for each question.</p>',
+          icon: "error",
+          customClass: {
+            title: "modal-font",
+          },
+          confirmButtonColor: "var(--incorrect-color)",
+        });
+      }
   });
 });
 
