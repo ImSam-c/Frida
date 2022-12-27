5091179d94429f5bb496eaf9fc8e90c65df41e38
Sam <samuelcorreos123@gmail.com>
2022-12-26 19:11:33 -0500

    fixing security breach

--- dist/helpers/variables.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.secretOrPrivateKey = exports.mongodbcnn = void 0;
-exports.mongodbcnn = "mongodb+srv://adminUser:OzgO7WrOuHoUYjRv@frida.8c1kloc.mongodb.net/test";
-exports.secretOrPrivateKey = "Pl3aS3GnR4T3An3%&!ASs1q378845";
